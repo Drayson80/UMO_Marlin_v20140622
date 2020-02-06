d4e89cb70387fdd21e71e8a2c2bc6e124a7e7ee3
Drayson80 <punisher01@gmx.net>
2020-02-06 16:19:34 +0100

    SD Sort - fix

--- Marlin/SdFatConfig.h
+++ Marlin/SdFatConfig.h
@@ -116,7 +116,7 @@ uint8_t const SOFT_SPI_SCK_PIN = 13;
 /** Number of VFAT entries used. Every entry has 13 UTF-16 characters */
 #define MAX_VFAT_ENTRIES (2)
 /** Total size of the buffer used to store the long filenames */
-##define LONG_FILENAME_LENGTH (FILENAME_LENGTH*MAX_VFAT_ENTRIES+1)
+#define LONG_FILENAME_LENGTH (FILENAME_LENGTH*MAX_VFAT_ENTRIES+1)
 #endif  // SdFatConfig_h
 
 
