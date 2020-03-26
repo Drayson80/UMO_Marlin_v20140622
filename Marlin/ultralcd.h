c3019740de1a3fc430403d21792e457c48e7719c
Drayson80 <punisher01@gmx.net>
2020-03-26 22:53:08 +0100

    Make Timeout configurable
    and extend to 30sec.

--- Marlin/ultralcd.h
+++ Marlin/ultralcd.h
@@ -21,9 +21,7 @@
 
   #define LCD_MESSAGEPGM(x) lcd_setstatuspgm(PSTR(x))
   #define LCD_ALERTMESSAGEPGM(x) lcd_setalertstatuspgm(PSTR(x))
-
   #define LCD_UPDATE_INTERVAL 100
-  #define LCD_TIMEOUT_TO_STATUS 15000
 
   #ifdef ULTIPANEL
   void lcd_buttons_update();
